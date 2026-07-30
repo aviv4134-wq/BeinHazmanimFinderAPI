d12f6f9b2715e1016388821a61204342842d94ae
avraham <aviv4134@gmail.com>
2026-07-30 12:02:31 +0300

    add interface croud to two repositoris

---
+++ BeinHazmanimFinderAPI/Repositorys/IAccommodation.cs
@@ -0,0 +1,18 @@
+﻿using System.Security.AccessControl;
+
+namespace BeinHazmanimFinderAPI.Repositorys;
+
+    public interface IAccommodation
+    {
+
+        public  Task<List<Accommodation>> GetAccommodationsAsync();
+        public Task<Accommodation>? GetByIdAsync(int id);
+        public Task<Accommodation> CreateAsync(Accommodation accommodation);
+        public Task< bool> UpdateAsync(int id, Accommodation accommodation);
+        public Task<bool> DeleteAsync(int id);
+        //public Task<List<Accommodation>> GetByMaxPriceCityAccessibleAsync(string? city = null, int? maxPrice = null,bool? accessible = null);
+
+        //public Task<List<string>> GetTypesAsync();
+
+}
+
---
+++ BeinHazmanimFinderAPI/Repositorys/IActivityPlace.cs
@@ -0,0 +1,13 @@
+﻿using BeinHazmanimFinderAPI;
+
+namespace BeinHazmanimFinderAPI.Repositorys
+{
+    public interface IActivityPlace
+    {
+        public Task<List<ActivityPlace>> GetActivityPlaceAsync();
+        public Task<ActivityPlace>? GetByIdAsync(int id);
+        public Task<ActivityPlace> CreateAsync(ActivityPlace activityPlace);
+        public Task<bool> UpdateAsync(int id, ActivityPlace activityPlace);
+        public Task<bool> DeleteAsync(int id);
+    }
+}
