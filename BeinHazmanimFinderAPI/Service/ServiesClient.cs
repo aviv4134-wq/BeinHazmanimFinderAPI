70496db86d03f525189bc8b441a5b683693c00a6
avraham <aviv4134@gmail.com>
2026-07-30 16:35:44 +0300

    add servis

---
+++ BeinHazmanimFinderAPI/Service/ServiesClient.cs
@@ -0,0 +1,14 @@
+﻿namespace BeinHazmanimFinderAPI
+{
+    public class ServiesClient : Iservies
+    {
+        private IAccommodation _accommodations;
+        private IActivityPlace _activityPlaces;
+
+        public ServiesClient(IAccommodation accommodation, IActivityPlace activityPlace)
+        {
+            _accommodations = accommodation;
+            _activityPlaces = activityPlace;
+        }
+    }
+}
