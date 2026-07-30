d138049d72c1570e784d59a91f9d5412f0d307e4
avraham <aviv4134@gmail.com>
2026-07-30 09:47:24 +0300

    add model Accommodation

---
+++ BeinHazmanimFinderAPI/Models/Accommodation.cs
@@ -0,0 +1,53 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BeinHazmanimFinderAPI.Models
+{
+    public class Accommodation
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(70)]
+
+        public string Name { get; set; } = string.Empty; //enum
+
+        [Required]
+        [StringLength(30)]
+        public string AccommodationType { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(40)]
+        public string City { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(50)]
+        public string Area { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(50)]
+        public string KashrutAuthority { get; set; } = string.Empty;
+
+        [Required]
+        [Range(0,10000)]
+        public decimal PricePerNight { get; set; }
+
+        [Required]
+        [Range(1,500)]
+
+        public int MaximumGuests { get; set; }
+
+        [Required]
+
+        public DateTime AvailableFrom { get; set; }
+
+        [Required]
+
+        public bool IsAccessible { get; set; } = false;
+
+        [Required]
+
+        public bool IsAbroad { get; set; } = false;
+
+    }
+}
